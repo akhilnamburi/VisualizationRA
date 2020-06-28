04e46553ee144a8978983d721090e55cce706591
akhil namburi <namburi.akhil12@gmail.com>
2020-06-28 16:39:24 -0500

    added matrix and node link graph

--- task1/index.js
+++ task1/index.js
@@ -7,8 +7,13 @@ app.use(express.static(__dirname + "/views"));
 
 var fs = require("fs");
 const path = require("path");
-
 var folder = path.join(__dirname, "text/");
+const { parseXML } = require("./chart.js");
+const txtData = fs.readFileSync("crescent.txt");
+const htmlToImage = require('html-to-image');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const bodyParser = require('body-parser');
 
 async function readFiles(dirname, onFileContent, onError) {
   filenames = fs.readdirSync(dirname);
@@ -30,12 +35,33 @@ readFiles(
   }
 );
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json());
+
 app.get("/", async (req, res) => {
   res.render("index", { files: data });
 });
 
 app.get("/readFile/:name", (req, res) => {
   res.send(data[req.params.name]);
+});
+
+app.get("/getAllData", (req, res) => {
+  parseXML(txtData).then(parsedData => {
+    fs.writeFileSync("graph.json", JSON.stringify(parsedData));
+    const dom = new JSDOM(fs.readFileSync(path.join(__dirname + '/graph.html')));
+    const $document = dom.window.document;
+    const content = $document.createElement('div');
+    
+    content.classList.add('matrix-content');
+    res.send('<div class="container" >  <div></div>  <div id="top"></div></div><div class="container">  <div id="side"></div>  <div id="matrix"></div></div>↵<svg  width="1920"  height="1080"  viewBox="0 0 1920 1080"  preserveAspectRatio="xMidYMid meet"></svg>');
+  });
 });
 
 app.listen(port, () =>
